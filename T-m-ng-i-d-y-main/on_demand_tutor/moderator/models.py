0f45a9c033f549bcc54ace454e883bf2ad6178d8
nhathuymario <huyne1501@gmail.com>
2024-12-29 14:56:14 +0700

    đã sữa moderator

---
+++ T-m-ng-i-d-y-main/on_demand_tutor/moderator/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Moderator(User):
+    def ModerateContent(self):
+        pass
+    
---
+++ T-m-ng-i-d-y-main/on_demand_tutor/tutor/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from on_demand_tutor.models import User  
+
+class Tutor(User):
+    def CreateService(self):
+        pass
+    def UploadContent(self):
+        pass
+    
