a82dc65c2262b51148a863cbc25ef69dfdeb57c0
nhathuymario <huyne1501@gmail.com>
2024-12-30 15:58:54 +0700

    Delete T-m-ng-i-d-y-main/on_demand_tutor directory

--- T-m-ng-i-d-y-main/on_demand_tutor/tutor/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from on_demand_tutor.models import User  
-
-class Tutor(User):
-    def CreateService(self):
-        pass
-    def UploadContent(self):
-        pass
-    
