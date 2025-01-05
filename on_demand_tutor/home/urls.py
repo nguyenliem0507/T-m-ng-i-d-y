7d43ae726f74dec36b383f83df60ddc9738fd4e7
nguyenliem0507 <nguyenliem9514@gmail.com>
2025-01-05 10:11:40 +0700

    Delete on_demand_tutor/home directory

--- on_demand_tutor/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from tutor import views  # Import views của bạn
-from .import view
-urlpatterns = [
-    path('', views.home, name= "home")
-]
