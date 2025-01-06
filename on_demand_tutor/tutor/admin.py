7265f533c88fc70b8817de2af5c6dd2374efff50
nguyenliem0507 <nguyenliem9514@gmail.com>
2025-01-06 08:45:34 +0700

    Update

--- on_demand_tutor/tutor/admin.py
+++ on_demand_tutor/tutor/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Tutor
 
 class TutorAdmin(admin.ModelAdmin):
-  list_display = ("FullName", "BirthDate", "Phone","Email")
+  list_display = ("FullName", "BirthDate", "Phone", "Email", "Gender", "Degree")
 admin.site.register(Tutor, TutorAdmin)
--- on_demand_tutor/tutor/models.py
+++ on_demand_tutor/tutor/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from on_demand_tutor.models import User  
 
 class Tutor(User):
+    Degree = models.CharField(max_length=255)
     def CreateService(self):
         pass
     def UploadContent(self):
         pass
     def __str__(self):
-        return f"{self.FullName} {self.BirthDate} {self.Phone} {self.Email}"
+        return f"{self.FullName} {self.BirthDate} {self.Phone} {self.Email} {self.Gender} {self.Degree} "
     
