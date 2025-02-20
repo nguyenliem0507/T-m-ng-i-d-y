9fe2ba97c2ce2062b4569cef617454029aa46f2e
HuynhhBao <hbao70018@gmail.com>
2025-02-20 13:24:47 +0700

    views.py tutor

--- on_demand_tutor/tutor/views.py
+++ on_demand_tutor/tutor/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
 def Tutor(request):
     return HttpResponse("Hello world!")
+
+def tutor_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('/tutor-login/')  # Chuyển hướng về trang đăng nhập của tutor
 
