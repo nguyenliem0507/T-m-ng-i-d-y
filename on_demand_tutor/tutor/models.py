24de2c8c309ecb361f30f410cd82517fe2f44b25
HuynhhBao <hbao70018@gmail.com>
2025-02-21 17:47:59 +0700

    update models.py tutor

--- on_demand_tutor/tutor/models.py
+++ on_demand_tutor/tutor/models.py
@@ -3,10 +3,13 @@ from on_demand_tutor.models import User
 
 class Tutor(User):
     Degree = models.CharField(max_length=255)
+
+    Subjects = models.CharField(max_length=255, blank=True, null=True)  # Thêm môn học
+    
     def CreateService(self):
         pass
     def UploadContent(self):
         pass
     def __str__(self):
-        return f"{self.FullName} {self.BirthDate} {self.Phone} {self.Email} {self.Gender} {self.Degree} "
-    
+        return f"{self.FullName} {self.BirthDate} {self.Phone} {self.Email} {self.Gender} {self.Subjects} {self.Degree}"
+   
